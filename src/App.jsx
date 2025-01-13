9a86c7c93493100e1c9655e1463fd290dd9028f7
Emily Yang <emilyyang@Emilys-MacBook-Pro.local>
2025-01-13 20:33:44 +0100

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>React | Thinking in React</h1>
+      <h1>React | Thinking in Reactss</h1>
     </div>
   );
 }
